54becbe7188eff2f8508e80f0ff577ae7d48633e
Dilnoza <dilnoza.033@gmail.com>
2019-01-19 14:36:21 -0500

    add new commit

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,10 +1,14 @@
 public class Calculator {
- public static double add(double num1,double num2){
-     return num1+num2;
- }
-  public static double subtraction(double num1,double num2){
-     return num1-num2;
-  }
- public static double multiply(double num1,double num2){
-  return num1 * num2;
+
+    public static double add(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    public static double subtraction(double num1, double num2) {
+        return num1 - num2;
+    }
+
+    public static double multiply(double num1, double num2) {
+        return num1 * num2;//multiple
+    }
 }
